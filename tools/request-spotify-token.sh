d798229ce073810719523f792d8fb4f079b7b002
Guido Domenici <gdomenici@verimatrix.com>
2024-11-08 21:43:10 +0100

    Added a few README.md instructions

--- tools/request-spotify-token.sh
+++ tools/request-spotify-token.sh
@@ -5,8 +5,16 @@
 # and
 # https://developer.spotify.com/dashboard/8ff46945b7414e13a7cabb0e9ac3ca8d/settings
 
-# Use playlist: https://open.spotify.com/playlist/1Bpgr72vuJwYXYqbdahtOO
+# Causes the .env variables to be set in the shell
+set -a && source .env && set +a
 
-curl -X POST "https://accounts.spotify.com/api/token" \
-     -H "Content-Type: application/x-www-form-urlencoded" \
-     -d "grant_type=client_credentials&client_id=8ff46945b7414e13a7cabb0e9ac3ca8d&client_secret=ba4f17e84dbb48c9ba8f3c3caa36caf6"
+
+# curl -X POST "https://accounts.spotify.com/api/token" \
+#      -H "Content-Type: application/x-www-form-urlencoded" \
+#      -d "grant_type=client_credentials&client_id=8ff46945b7414e13a7cabb0e9ac3ca8d&client_secret=ba4f17e84dbb48c9ba8f3c3caa36caf6"
+
+SPOTIFY_RESPONSE=$(curl -X POST "https://accounts.spotify.com/api/token" \
+      -H "Content-Type: application/x-www-form-urlencoded" \
+      -d "grant_type=client_credentials&client_id=${SPOTIFY_CLIENT_ID}&client_secret=${SPOTIFY_CLIENT_SECRET}")
+
+echo "$SPOTIFY_RESPONSE" | jq ".access_token"
