0f3a4ab09d2dd481bcb09bd9dd5aaa2347db4435
Guido Domenici <gdomenici@verimatrix.com>
2024-11-08 21:44:10 +0100

    Removed some sensitive info

--- tools/request-spotify-token.sh
+++ tools/request-spotify-token.sh
@@ -8,11 +8,6 @@
 # Causes the .env variables to be set in the shell
 set -a && source .env && set +a
 
-
-# curl -X POST "https://accounts.spotify.com/api/token" \
-#      -H "Content-Type: application/x-www-form-urlencoded" \
-#      -d "grant_type=client_credentials&client_id=8ff46945b7414e13a7cabb0e9ac3ca8d&client_secret=ba4f17e84dbb48c9ba8f3c3caa36caf6"
-
 SPOTIFY_RESPONSE=$(curl -X POST "https://accounts.spotify.com/api/token" \
       -H "Content-Type: application/x-www-form-urlencoded" \
       -d "grant_type=client_credentials&client_id=${SPOTIFY_CLIENT_ID}&client_secret=${SPOTIFY_CLIENT_SECRET}")
