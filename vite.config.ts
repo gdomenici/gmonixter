042d6adac6b158bb7d7ff0db022f02c8f1e3592b
gdomenici <guido.domenici@gmail.com>
2024-11-09 19:01:58 +0100

    Ensure authorize.html is present in distro

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,15 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      input: {
+        main: path.resolve(__dirname, "index.html"),
+        authorize: path.resolve(__dirname, "authorize.html"), // New HTML file entry point
+      },
+    },
+    target: "esnext", // Ensures support for top-level await
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
